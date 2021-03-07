ace7684266d04a6a7c2a14ad0b1a407ad3137a71
Emil <emilfigzal@gmail.com>
2021-03-07 13:46:35 +0100

    03 receive POJO by RabbitMQ

--- src/main/java/pl/emil7f/receiver/MessageController.java
+++ src/main/java/pl/emil7f/receiver/MessageController.java
@@ -1,9 +1,10 @@
 package pl.emil7f.receiver;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.emil7f.notification.Notification;
 
 @RestController
 public class MessageController {
@@ -24,8 +25,20 @@ public class MessageController {
         }
     }
 
-    @RabbitListener(queues = "myQueue")
-    public void listenerMessage(String message) {
-        System.out.println(message);
+//    @RabbitListener(queues = "myQueue")
+//    public void listenerMessage(String message) {
+//        System.out.println(message);
+//    }
+
+    @GetMapping("/notification")
+    public ResponseEntity<Notification> receiveNotification() {
+        Object notification = rabbitTemplate.receiveAndConvert("myQueue");
+        if (notification instanceof Notification) {
+            return ResponseEntity.ok((Notification) notification);
+        }else {
+            return ResponseEntity.noContent().build();
+        }
     }
+
+
 }
